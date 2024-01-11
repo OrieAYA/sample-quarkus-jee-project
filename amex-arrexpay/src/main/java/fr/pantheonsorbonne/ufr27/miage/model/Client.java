1b1e6f8e9f6eb487f0a63e9184225eebce7af640
OrieAYA <screpin.du.64@gmail.com>
2024-01-11 16:49:40 +0100

    Model

---
+++ amex-arrexpay/src/main/java/fr/pantheonsorbonne/ufr27/miage/model/Client.java
@@ -0,0 +1,32 @@
+package fr.pantheonsorbonne.ufr27.miage.model;
+import jakarta.persistence.*;
+
+@Entity
+public class Client {
+    @Id
+    @Column(name = "idClient", nullable = false)
+    private Integer idClient;
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer num_carte;
+
+    private Float montant_argent;
+
+    public Client(Integer idClient) {
+        this.idClient = idClient;
+    }
+
+    public Client() {}
+
+    public Integer getNum_carte(){return num_carte;}
+
+    public void setNum_carte(Integer num_carte){this.num_carte = num_carte;}
+
+    public Integer getIdClient(){return idClient;}
+
+    public void setIdClient(Integer idClient){this.idClient=idClient;}
+
+    public Float getMontantArgent(){return montant_argent;}
+
+    public  void setMontantArgent(Float montant_argent){this.montant_argent=montant_argent;}
+}
