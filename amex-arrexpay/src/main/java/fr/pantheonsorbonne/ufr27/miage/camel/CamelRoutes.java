252140af6b5f70735bda342bc0822d08eed6e425
OrieAYA <screpin.du.64@gmail.com>
2024-01-14 14:54:56 +0100

    Nvm

--- amex-arrexpay/src/main/java/fr/pantheonsorbonne/ufr27/miage/camel/CamelRoutes.java
+++ amex-arrexpay/src/main/java/fr/pantheonsorbonne/ufr27/miage/camel/CamelRoutes.java
@@ -1,13 +1,7 @@
 package fr.pantheonsorbonne.ufr27.miage.camel;
 
-import fr.pantheonsorbonne.ufr27.miage.dto.Booking;
-import fr.pantheonsorbonne.ufr27.miage.dto.ETicket;
-import fr.pantheonsorbonne.ufr27.miage.model.CashBack;
-import fr.pantheonsorbonne.ufr27.miage.model.Client;
 import org.apache.camel.CamelContext;
-import org.apache.camel.CamelExecutionException;
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
+
 import org.apache.camel.builder.RouteBuilder;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
@@ -38,24 +32,4 @@ public class CamelRoutes extends RouteBuilder {
     public void configure() throws Exception {
 
         camelContext.setTracing(true);
-
-        from("direct:M1.AMEX.AskTaux")
-                .log("Received request: ${body}")
-                .process(exchange -> {
-                    LinkedHashMap<String, Object> jsonMap = exchange.getIn().getBody(LinkedHashMap.class);
-                    CashBack cashBack = new CashBack();
-                    cashBack.setIdClient((Integer) jsonMap.get("idClient"));
-                    cashBack.setIdCashback((Integer) jsonMap.get("idCashback"));
-                    cashBack.setIdTransaction((Integer) jsonMap.get("idTransaction"));
-                    cashBackGateway.cashBack(cashBack);
-                    exchange.setProperty("cashBack", cashBack);
-                    exchange.setProperty("idClient", cashBack.getIdClient());
-                })
-                .log("Sending reply: ${body}")
-                .to("direct:sendReplyAmex");
-
-        from("direct:sendReplyAmex")
-                .log("Sent reply: ${body}");
-
     }
-}
